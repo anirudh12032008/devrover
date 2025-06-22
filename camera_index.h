93717d4638a8a845b277144f4e08efc57871587b
Anirudh Sahu <renudilip19@gmail.com>
2025-06-23 01:13:53 +0530

    Create camera_index.h

---
+++ camera_index.h
@@ -0,0 +1,70 @@
+#include "esp_http_server.h"
+
+esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len;
+  uint8_t * _jpg_buf;
+  char * part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if(res != ESP_OK){
+    return res;
+  }
+
+  while(true){
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if(fb->format != PIXFORMAT_JPEG){
+        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+        if(!jpeg_converted){
+          Serial.println("JPEG compression failed");
+          esp_camera_fb_return(fb);
+          res = ESP_FAIL;
+        }
+      } else {
+        _jpg_buf_len = fb->len;
+        _jpg_buf = fb->buf;
+      }
+
+      if(res == ESP_OK){
+        size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+        res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+      }
+
+      if(res == ESP_OK){
+        res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+      }
+
+      if(fb->format != PIXFORMAT_JPEG){
+        free(_jpg_buf);
+      }
+      esp_camera_fb_return(fb);
+    }
+
+    if(res != ESP_OK){
+      break;
+    }
+  }
+
+  return res;
+}
+
+void startCameraServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+
+  httpd_uri_t stream_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = stream_handler,
+    .user_ctx  = NULL
+  };
+
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &stream_uri);
+  }
+}
